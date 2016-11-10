ae481fd904a7146df79869cc42f5bd8aa3a8f935
shinji <rill0131@gmail.com>
2016-11-11 02:30:50 +0900

    Sierpinski gasket practice

--- 1_animation/scripts/index.js
+++ 1_animation/scripts/index.js
@@ -1,17 +1,48 @@
 window.addEventListener("load", init);
-    function init() {
-      // Stageオブジェクトを作成。表示リストのルートになります。
-      var stage = new createjs.Stage("myCanvas");
-      // シェイプを作成
-      var shape = new createjs.Shape();
-      // 塗りの色を指定
-      shape.graphics.beginFill("DarkRed");
-      // 円を描く
-      shape.graphics.drawCircle(0, 0, 80);
-      stage.addChild(shape); // 表示リストに追加
-      // 配置座標を設定
-      shape.x = 200;
-      shape.y = 100;
-      // Stageの描画を更新
-      stage.update();
+
+  function init(){
+
+    var stage = new createjs.Stage("myCanvas");
+
+    var shape = new createjs.Shape();
+    stage.addChild(shape);
+
+    var SIZE = 600;
+
+    var HEIGHT = SIZE * Math.sin(60 * Math.PI / 180);
+
+    drawTri(5,0,HEIGHT, SIZE / 2, 0, SIZE, HEIGHT);
+
+    function drawTri(level,
+                      x1,y1,
+                      x2,y2,
+                      x3,y3){
+
+      if(level <= 0){
+        shape.graphics.beginStroke("DarkRed").setStrokeStyle(2.0);
+        shape.graphics.moveTo(x1, y1);
+        shape.graphics.lineTo(x2, y2);
+        shape.graphics.lineTo(x3, y3);
+        shape.graphics.closePath();
+      }else{
+        var nx1 = (x1 + x2) / 2;
+        var ny1 = (y1 + y2) / 2;
+
+        var nx2 = (x2 + x3) / 2;
+        var ny2 = (y2 + y3) / 2;
+
+        var nx3 = (x3 + x1) / 2;
+        var ny3 = (y3 + y1) / 2;
+
+        level = level - 1;
+
+        drawTri(level, x1, y1, nx1, ny1, nx3, ny3);
+        drawTri(level, x2, y2, nx1, ny1, nx2, ny2);
+        drawTri(level, x3, y3, nx2, ny2, nx3, ny3);
+      }
     }
+
+    stage.update();
+
+
+  }
