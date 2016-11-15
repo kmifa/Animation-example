407ef67d6897a33cf8b3954a0f3ed02d75c9a90f
shinji <rill0131@gmail.com>
2016-11-15 21:17:00 +0900

    Sierpinski gasket

--- 1_animation/scripts/index.js
+++ 1_animation/scripts/index.js
@@ -7,24 +7,37 @@ window.addEventListener("load", init);
     var shape = new createjs.Shape();
     stage.addChild(shape);
 
-    var SIZE = 600;
+    var SIZE = 220;
 
     var HEIGHT = SIZE * Math.sin(60 * Math.PI / 180);
 
-    drawTri(5,0,HEIGHT, SIZE / 2, 0, SIZE, HEIGHT);
+    var margin = SIZE + 20;
 
-    function drawTri(level,
-                      x1,y1,
-                      x2,y2,
-                      x3,y3){
+    for(var i = 0; i < 8; i++){
 
+      var offsetX = (i % 4) * margin;
+      var offsetY = Math.floor(i / 4) * margin;
+
+      drawTriangle(i,
+            0 + offsetX, HEIGHT + offsetY,
+            SIZE / 2 + offsetX, 0 + offsetY,
+            SIZE + offsetX, HEIGHT + offsetY);
+    }
+
+    function drawTriangle(level,
+                          x1, y1,
+                          x2, y2,
+                          x3, y3){
       if(level <= 0){
-        shape.graphics.beginStroke("DarkRed").setStrokeStyle(2.0);
-        shape.graphics.moveTo(x1, y1);
-        shape.graphics.lineTo(x2, y2);
-        shape.graphics.lineTo(x3, y3);
+
+        shape.graphics.beginStroke("DarkRed").setStrokeStyle(0.5);
+
+        shape.graphics.moveTo(x1,y1);
+        shape.graphics.lineTo(x2,y2);
+        shape.graphics.lineTo(x3,y3);
         shape.graphics.closePath();
       }else{
+
         var nx1 = (x1 + x2) / 2;
         var ny1 = (y1 + y2) / 2;
 
@@ -36,9 +49,9 @@ window.addEventListener("load", init);
 
         level = level - 1;
 
-        drawTri(level, x1, y1, nx1, ny1, nx3, ny3);
-        drawTri(level, x2, y2, nx1, ny1, nx2, ny2);
-        drawTri(level, x3, y3, nx2, ny2, nx3, ny3);
+        drawTriangle(level, x1, y1, nx1, ny1, nx3, ny3);
+        drawTriangle(level, x2, y2, nx1, ny1, nx2, ny2);
+        drawTriangle(level, x3, y3, nx2, ny2, nx3, ny3);
       }
     }
 
